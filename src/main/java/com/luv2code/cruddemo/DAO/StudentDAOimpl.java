08174b709540baa6ab8de066c8093d5e46ad2948
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-23 14:38:04 -0300

    Created DAO Student impl

---
+++ src/main/java/com/luv2code/cruddemo/DAO/StudentDAOimpl.java
@@ -0,0 +1,26 @@
+package com.luv2code.cruddemo.DAO;
+
+import com.luv2code.cruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentDAOimpl implements StudentDAO{
+    //define field for Entity Manager
+    private EntityManager entityManager;
+
+    // inject entity manager using constructor injection
+    @Autowired
+    public StudentDAOimpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    // implement save method
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+        entityManager.persist(theStudent);
+    }
+}
