f30b2f5293dfba759437a9b9a9a3580537eb3367
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-24 20:14:56 -0300

    Created method to delete all of students by id

--- src/main/java/com/luv2code/cruddemo/dao/StudentDAOimpl.java
+++ src/main/java/com/luv2code/cruddemo/dao/StudentDAOimpl.java
@@ -62,4 +62,11 @@ public class StudentDAOimpl implements StudentDAO{
         Student theStudent = entityManager.find(Student.class, id);
         entityManager.remove(theStudent);
     }
+
+    @Override
+    @Transactional
+    public int deleteAll() {
+        int numRowsDeleted = entityManager.createQuery("DELETE FROM Student").executeUpdate();
+        return numRowsDeleted;
+    }
 }
